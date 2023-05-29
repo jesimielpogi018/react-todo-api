23e0aee79a6e0ca25a2b1302c9bf68f82da6576b
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-05-29 11:08:13 +0800

    fixed get specific todo list, and added middleware to validate request body

--- todos/todos.js
+++ todos/todos.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const DB = require("../db");
 
 // middleware
 const { noEmptyReqBody } = require("../middlewares/noEmptyReqBody");
+const { validateAddTodoReqBody } = require("../middlewares/validateAddTodoReqBody");
 
 // mongodb uri
 const uri = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
@@ -22,34 +24,59 @@ router.get("/", (req, res, next) => {
   conn
     .connect()
     .then((client) => {
-      const collection = client.db("user").collection("users");
-      const data = collection.find().toArray();
-      res.json(data);
+      const collection = client.db(DB.DB).collection(DB.TODOS);
+      return collection.find({}).toArray();
+    })
+    .then((data) => {
+      res.status(200).json(data);
     })
     .catch((err) => {
       console.log(err);
       res.status(400).json({
         message: "Error Ocurred!",
-        error: err
+        error: err,
       });
     });
 });
 
-app.get("/{id}", (req, res, next) => {
-
-});
-
-// add todos
-router.post("/", noEmptyReqBody, (req, res, next) => {
-  const { username, pwd } = req.body;
+// get todos
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  const query = { _id: new ObjectId(id) };
 
   conn
     .connect()
     .then((client) => {
-      const collection = client.db("user").collection("users");
+      const collection = client.db(DB.DB).collection(DB.TODOS);
+      return collection.findOne(query);
+    })
+    .then((data) => {
+      res.json({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({
+        message: "Error Ocurred!",
+        error: err,
+      });
+    });
+});
+
+// add todos
+router.post("/", noEmptyReqBody, validateAddTodoReqBody, (req, res, next) => {
+  const { todoTask, isStarred, todoDueDate, todoDetails, todoCreatedAt } = req.body;
+
+  conn
+    .connect()
+    .then((client) => {
+      const collection = client.db(DB.DB).collection(DB.TODOS);
       const data = {
-        username,
-        pwd,
+        todoTask,
+        isStarred,
+        todoCreatedAt,
+        todoUpdatedAt: todoCreatedAt,
+        todoDueDate,
+        todoDetails,
       };
       return collection.insertOne(data);
     })
@@ -57,21 +84,24 @@ router.post("/", noEmptyReqBody, (req, res, next) => {
       if (result.acknowledged) {
         res.status(200).json({
           message: "Inserted User Successfully!",
-          _id: result.insertedId
-        })
+          _id: result.insertedId,
+        });
       }
     })
     .catch((err) => {
       console.log(err);
       res.status(400).json({
         message: "Error Ocurred!",
-        error: err
+        error: err,
       });
     });
 });
 
 // edit todos
 router.patch("/", noEmptyReqBody, (req, res, next) => {});
+
+// overwrite todos
+router.put("/", noEmptyReqBody, (req, res, next) => {});
 
 // delete todos
 router.delete("/", noEmptyReqBody, (req, res, next) => {});
