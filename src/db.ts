987bb40baf84d31028e58f0027dcb3db268ba5ba
Jesimiel Almedejar <jesimielalmedejar1018@gmail.com>
2023-05-30 09:42:41 +0800

    added interface type for betting type and error handling

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,15 @@
-module.exports = {
+interface DB {
+  DB: string,
+  TODOS: string,
+  USERS: string,
+  AUTH: string,
+}
+
+const db: DB = {
   DB: "todos",
   TODOS: "todo_lists",
   USERS: "todo_users",
   AUTH: "todo_users_auth",
 };
+
+module.exports = db;
